29a343d1f8b23ecb71b149d8a90b0d1808fd4d1b
jwwishart <jwwishart>
2016-09-03 12:06:16 +1000

    initial structure

---
+++ erg-compiler/Compiler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace erg.compiler
+{
+
+    public class CompilerResult {
+        public bool Success = false;
+    }
+    
+
+    public class Compiler
+    {
+        public CompilerResult Compile()
+        {    
+            return new CompilerResult {
+                Success = true
+            };
+        }
+    }
+}
---
+++ erg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("erg 0 (c) 2016 Justin Wishart");
+        }
+    }
+}
