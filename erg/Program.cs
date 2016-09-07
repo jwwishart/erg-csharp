ed00505d261e1617f5222cd51e2a714a5a6d8c3f
jwwishart <jwwishart>
2016-09-07 22:15:37 +1000

    Got the parsing for numeric expressions and basic infix operators

--- erg/Program.cs
+++ erg/Program.cs
@@ -4,6 +4,9 @@ using System.IO;
 
 namespace ConsoleApplication
 {
+
+
+    // TODO create customer parser exception functions instead of using ArgumentException
     public class Program
     {
         public static void Main(string[] args)
@@ -25,14 +28,30 @@ namespace ConsoleApplication
 
             var tokens = Lex(code);
             foreach (var token in tokens) {
-                Console.WriteLine(token.Type.ToString());
+                Console.WriteLine(token.Type.ToString() + ": " + token.Value);
             }
             if (tokens.Count == 0) {
                 Console.WriteLine("no tokens");
             }
 
             // Build AST
-            var ast = Parse(tokens);
+            ParseResult result;
+            var context = new ParseContext();
+            context.Tokens = tokens;
+            context.Program = new ProgramNode();
+
+            try {
+                result = Parse(context);
+            } catch (ArgumentException ex) {
+                Console.WriteLine("Parser Error: " + ex.Message);
+                result = new ParseResult { Success = false };
+            }
+
+            if (result.Success) {
+                Console.WriteLine("YAY!!!");
+            } else {
+                Console.WriteLine(":'o(");
+            }
         }
 
         public abstract class AstNode 
@@ -44,36 +63,33 @@ namespace ConsoleApplication
         {
             public override AstNodeType Type => AstNodeType.Program;
 
+            public List<AstNode> Files = new List<AstNode>();
+        }
+
+        public class FileNode: AstNode {
+            public override AstNodeType Type => AstNodeType.File;
             public List<AstNode> Body;
         }
 
         public class ExpressionStatement : AstNode {
             public override AstNodeType Type => AstNodeType.ExpressionStatement;
 
-            public AstNode Left;
+            public AstNode Left = null;
 
-            public AstNode Right;
+            public AstNode Right = null;
         }
 
 
 
         public enum AstNodeType {
             Program,
+            File,
             ExpressionStatement,
             BinaryExpression,
         }
 
-        public static ParseResult Parse(List<Token> tokens) {
-            var context = new ParseContext();
-
-            while (context.Index > context.Tokens.Count - 1) {
-                var result = Parse_Statement(context);
-                if (result.Success == false) {
-                    return result;
-                }
-            }
-
-            return new ParseResult();
+        public static ParseResult Parse(ParseContext context) {
+            return Parse_Program(context);
         }
 
         public class ParseContext {
@@ -95,15 +111,40 @@ namespace ConsoleApplication
 
         public static bool Expect(ParseContext context, TokenType type) {
             if (context.Tokens[context.Index].Type != type) {
+                // TODO is an exception here a good idea... 
+                // I reallllyyyyyy don't like it...
+                throw new ArgumentException(
+                    "Expected token of type " + type.ToString() + 
+                    " but got one of type " + context.Tokens[context.Index].Type.ToString());
+
                 return false;
             }
 
             return true;
         }
 
-        public static ParseResult Parse_Statement(ParseContext context) {
+        public static ParseResult Parse_Program(ParseContext context) {
             context.Program = new ProgramNode();
 
+            return Parse_File(context);
+        }
+
+        public static ParseResult Parse_File(ParseContext context) {
+            var file = new FileNode();
+            context.Program.Files.Add(file);
+            context.Current = file;
+
+            while (context.Index <= context.Tokens.Count - 1) {
+                var result = Parse_Statement(context);
+                if (result.Success == false) {
+                    return result;
+                }
+            }
+
+            return new ParseResult();
+        }
+
+        public static ParseResult Parse_Statement(ParseContext context) {
             // The destinction between a statement and an expression statement
             // is that the statement doesn't actually return anything... so 
             // the whole statement would not evaluate to something... but that is strange...
@@ -116,17 +157,70 @@ namespace ConsoleApplication
 
         public static ParseResult Parse_Expression(ParseContext context) {
             // expression = ["+"|"-"] term {("+"|"-") term} .
-            return Parse_Term(context);
+
+            // TODO store the AST
+            if (Accept(context, TokenType.Add) || Accept(context, TokenType.Subtract)) {
+                context.Index++;
+            }
+
+            var term = Parse_Term(context);
+
+            if (term.Success) {
+                if (Accept(context, TokenType.Add) || Accept(context, TokenType.Add)) {
+                    context.Index++;
+                    var term2 = Parse_Term(context);
+                    if (term2.Success) {
+                        return term2;
+                    }
+
+                    throw new ArgumentException("Parse Error: Expected infix operator provided but not subsequent term .");
+                }
+
+                return term;
+            }
+
+            throw new ArgumentException("Parse Error: Expected expression = [\"+\"|\"-\"] term {(\"+\"|\"-\") term} .");
         }
 
         public static ParseResult Parse_Term(ParseContext context) {
             // term = factor {("*"|"/") factor} .
-            return Parse_Factor(context);
+            var factor = Parse_Factor(context);
+            
+            if (factor.Success == false) {
+                return factor;
+            }
+
+            if (Accept(context, TokenType.Multiply)) {
+                context.Index++;
+                return Parse_Factor(context);
+            }
+
+            if (Accept(context, TokenType.Divide)) {
+                context.Index++;
+                return Parse_Factor(context);
+            }
+
+            // A factor might just be a number... there might be + or - after This
+            // so this Term is done!!!
+            return factor;
         }
 
         public static ParseResult Parse_Factor(ParseContext context) {
-           // factor = ident | number | "(" expression ")" .
-           return new ParseResult();
+            // factor = ident | number | "(" expression ")" .
+
+            // Numbers could be prefixed with - or +
+            if (Accept(context, TokenType.Add) || Accept(context, TokenType.Subtract)) {
+                context.Index++;
+
+                Expect(context, TokenType.LiteralNumber);
+            }
+
+            if (Accept(context, TokenType.LiteralNumber)) {
+                context.Index++;
+                return new ParseResult();
+            }
+
+            throw new ArgumentException("Parse Error: Expected factor = ident | number | \"(\" expression \")\" .");
         }
 
         public enum TokenType {
@@ -136,7 +230,10 @@ namespace ConsoleApplication
             LiteralNumber,
 
             // Operators
-            Plus
+            Add,
+            Subtract,
+            Multiply,
+            Divide
         }
 
         public class Token {
@@ -152,6 +249,10 @@ namespace ConsoleApplication
                 if (index > code.Length - 1) break;
                 
                 switch (code[index]) {
+                    case ' ':
+                        // Basically ignore spaces
+                        index++;
+                        continue;
                     case '0':
                     case '1':
                     case '2':
@@ -162,13 +263,23 @@ namespace ConsoleApplication
                     case '7':
                     case '8':
                     case '9':
-                        // TODO inefficient :oS
-                        result.Add(new Token { Type = TokenType.LiteralNumber, Value = code[index].ToString() });
-                        index++;
+                        GetNumber(code, ref index, result);
                         continue;
 
                     case '+':
-                        result.Add(new Token { Type = TokenType.Plus });
+                        result.Add(new Token { Type = TokenType.Add });
+                        index++;
+                        continue;
+                    case '-':
+                        result.Add(new Token { Type = TokenType.Subtract });
+                        index++;
+                        continue;
+                    case '*':
+                        result.Add(new Token { Type = TokenType.Multiply });
+                        index++;
+                        continue;
+                    case '/':
+                        result.Add(new Token { Type = TokenType.Divide });
                         index++;
                         continue;
                 }
@@ -177,6 +288,37 @@ namespace ConsoleApplication
             }
 
             return result;
+        }
+
+        private static void GetNumber(string code, ref int index, IList<Token> results) {
+            var numberToken = new Token { Type = TokenType.LiteralNumber };
+
+            while (true) {
+                if (index > code.Length - 1) break;
+
+                switch (code[index]) {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        // TODO PERF string builder in context that 
+                        // retains it's capacity and is re-used 
+                        // instead of repeated string concatention
+                        numberToken.Value += code[index];
+                        index++;
+                        continue;
+                }
+
+                break;
+            }
+
+            results.Add(numberToken);
         }
     }
 }
